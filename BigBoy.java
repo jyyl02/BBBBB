a288de47bcd38de94e9bc554650903cf8fc86d55
jyyl02 <39536235+jyyl02@users.noreply.github.com>
2018-06-06 16:30:43 -0700

    Update BigBoy.java

--- BigBoy.java
+++ BigBoy.java
@@ -1,167 +1,204 @@
 /**
- * Write a description of class GameMain here.
- * 
- * Joy Liu P5, Isabella Wu P6
+ * Write a description of class BigBoy here.
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import javafx.scene.input.KeyCode;
-import javafx.animation.Animation;
-
-public class GameMain extends Application
+public class BigBoy
 {
-     // private instance variables
-    private int screenWidth, screenHeight;
-    private Menu menu;
-    private Frame frame;
-    private Level level; //not needed????**
-    private BigBoy bigBoy;
+    //instance variables
+    private int lives, cherries; 
+    private int dx; // x displacement
+    private int bx1, bx2; //background image one + two *****
+    private int x, y; //x and y location
+    Image bB;
+    //figure out images
+        ImageIcon facingLeft = new ImageIcon("bigboy-right.png");
+        //ImageIcon facingRight = new ImageIcon("");
+        //ImageIcon jumping = new ImageIcon("");
     
-    private int gameState;
-    private int MENU, PLAYING, PAUSED, WON;
-    
-    public GameMain()
+    /**
+     * Constructor for objects of class BigBoy
+     */
+    public BigBoy()
     {
-        screenWidth = 750;
-        screenHeight = 500;
+        //adjust values
+        x = 50;
+        y = 120; //on top of 2 rows of blocks (60 ea) 
+        bx1 = 0;
+        bx2 = -750;
         
-        //ImageIcon i = newImageIcon("file");
-        //menuBg = i.getImage();
-        //i = new ImageIcon("file");
-        //gameBg = i.getImage();
-        //bigBoy = new BigBoy(0, 0); //adjust values;
-        menu = new Menu();
         
-        MENU = 0;
-        PLAYING = 1;
-        PAUSED = 2;
-        WON = 3;
-        gameState = MENU;
-        //level = 0; //tutorial
+        //bB = facingRight.getImage();
     }
     
-    @Override 
-    public void start(Stage stage) 
+    //changes position left/right
+    public void run(boolean right)
     {
-        Canvas canvas = new Canvas(screenWidth, screenHeight);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
-        tl.setCycleCount(Timeline.INDEFINITE);
-        canvas.setFocusTraversable(true);
-        
-        
-        //handle mouse and key events
-        canvas.setOnKeyPressed( e ->
+        if(x > 0 && x < 750)
         {
-            if(e.getCode() == KeyCode.LEFT)
+            if(right)
             {
-                bigBoy.run(false);
+                //bB = facingRight.getImage();
+                x += dx;
             }
-            
-            if(e.getCode() == KeyCode.RIGHT)
+            else
             {
-                bigBoy.run(true);
+                //bB = facingLeft.getImage();
+                x -= dx;
             }
-            
-            if(e.getCode() == KeyCode.UP)
-            {
-                bigBoy.jump();
-            }
-        });
-        
-        canvas.setOnKeyReleased( e ->
-        {
-            if(e.getCode() == KeyCode.LEFT || e.getCode() == KeyCode.RIGHT)        
-                bigBoy.setDx(0);
-        });
-        
-        canvas.setOnMouseClicked(e ->
-        {
-            int mouseX = (int) e.getX();
-            int mouseY = (int) e.getY();
-            if(gameState == MENU)
-            {
-                /* if mouse is clicked within the "START" button
-                if(mouseX >= xx && mouseX <= xx && mouseY >= yy && mouseY <= yy)
-                {
-                    gameState = PLAYING;
-                }
-                */
-            }
-            
-            if(gameState == PAUSED)
-            {
-                tl.play();
-                gameState = PLAYING;
-            }
-            else if(gameState == PLAYING)
-            {
-                tl.pause();
-                gameState = PAUSED;
-            }
-        });
-        
-        stage.setTitle("BBBBB");
-        stage.setScene(new Scene(new StackPane(canvas)));
-        stage.show();
-        tl.play(); 
-    }
-
-    private void run(GraphicsContext gc)
-    {
-        // color for background
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, 750, 500);
-        
-        //test- not working
-        //***** figures out images!
-        //File file = new File("file:src/bigboy-right.png");
-        Image image = new Image("/bigboy-right.png");
-        gc.drawImage(image, 100, 100);
-        
-        // objects on screen
-         if(gameState == MENU)
-        {
-            //gc.drawImage(menuBg, 0, 0);
-        }
-        else if(gameState != WON)
-        {
-            //gc.drawImage(gameBg, 0, 0);
-            //gc.drawImage(gameBg, -750, 0); ??????
-            gc.setFill(Color.WHITE);
-            //score
-            gc.fillText("" + bigBoy.getCherries() * 5, screenWidth - 50, screenHeight / 8); //adjust values
-            //cherry count (collected/total)
-            gc.fillText(bigBoy.getCherries() + "/" + level.getCherries(), screenWidth / 2,
-            screenHeight / 8);
-            //lives
-            gc.fillText("" + bigBoy.getLives(), screenWidth / 5, screenHeight / 8);
         }
         
+        /*
+        if(x <= 0)
+        {
+            x = 1;
+            bx1 += dx / 2; //slows down bg speed
+            bx2 += dx / 2;
+        }
+        
+        if(x >= 750)
+        {
+            x = 749;
+            bx1 += dx / 2; //slows down bg speed
+            bx2 += dx / 2;
+        }
+        */
+       
+        //need??
     }
     
-
-    // run program
-    public static void main(String[] args)
+    //changes position based on up, right/left arrow keys
+    public  void jump()
     {
-        Application.launch(args);
+        if(checkCollisions() == 1) //if x/y value is on a block
+        {
+            
+        }
+        
+        while(checkCollisions() != 1) //while x/y value is in the air
+        {
+            y -= 2; //adjust value
+        }
     }
+    
+    public int checkCollisions()
+    {
+        //if bigboy x position is hitting a block
+        //return 0
+        // if y pos hitting block 
+        //return 1
+        //if cherry hit
+        //return 2
+        
+        return -1;
+    }
+    
+    //collects cherry
+    public void collectCherry()
+    {
+        cherries++;
+    }
+    
+    //reduces health by one (bitten by snake)
+    public void takeHit()
+    {
+        lives--;
+    }
+    
+    //jumps in the same direction as the snake attack
+    public void jumpBack()
+    {
+        
+    }
+    
+    //called when new level is started, or if level is restarted
+    public void fullLives()
+    {
+        lives = 3;
+    }
+    
+    public void setDx(int x)
+    {
+        dx = x;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    } 
+    
+        public int getCherries()
+    {
+        return cherries;
+    }
+    
+    public int getLives()
+    {
+        return lives;
+    }
+    
+    public Image getImage()
+    {
+        return bB;
+    }
+
+    
+    
+    
+    /*
+    public void keyPressed(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+        
+        if(key == KeyEvent.VK_LEFT)
+        {
+            dx = -2; //adjust values
+        }
+        
+        if(key == KeyEvent.VK_RIGHT)
+        {
+            dx = 2; //adjust values
+        }
+        
+        if(key == KeyEvent.VK_UP)
+        {
+            dy = 2; //>???? adjust values
+        }
+    }
+    
+   
+   
+    public void keyReleasesd (KeyEvent e)
+    {
+        int key = e.getKeyCode();
+        
+        if(key == KeyEvent.VK_LEFT)
+        {
+            dx = 0; 
+        }
+        
+        if(key == KeyEvent.VK_RIGHT)
+        {
+            dx = 0; 
+        }
+        
+        /*
+        if(key == KeyEvent.VK_UP)
+        {
+            dy = 0; //idk
+        }
+        
+    }
+    
+    */
 }
